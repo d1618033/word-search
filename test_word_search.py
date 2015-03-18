32160a5bb1c053e694596fb0bfb5f45a0a7ccc17
d1618033 <d1618033@gmail.com>
2015-03-18 21:35:32 +0200

    add helper method to loop over all cells and directions

--- test_word_search.py
+++ test_word_search.py
@@ -5,6 +5,14 @@ class TestLettersInDir(unittest.TestCase):
     def setUp(self):
         self.board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
+    def assertAllCells(expected_func, n):
+        for i in range(3):
+            for j in range(3):
+                for direction in word_search.DIRECTIONS:
+                    expected = expected_func(self.board[i][j])
+                    actual = word_search.letters_in_dir(self.board, i, j, n, direction)
+                    self.assertEqual(actual, expected)
+
     def test_up_2_in_middle(self):
         i , j = 1, 2
         n = 2
@@ -15,12 +23,7 @@ class TestLettersInDir(unittest.TestCase):
 
     def test_n_1_always_returns_number(self):
         n = 1
-        for i in range(3):
-            for j in range(3):
-                for direction in word_search.DIRECTIONS:
-                    expected = [self.board[i][j]]
-                    actual = word_search.letters_in_dir(self.board, i, j, n, direction)
-                    self.assertEqual(actual, expected)
+        self.assertAllCells(lambda cell: [cell], n=1)
 
     def test_up_2_at_top(self):
         i , j = 0, 1

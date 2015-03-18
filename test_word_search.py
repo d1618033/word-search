3de1dd354934d3e8a56a436767880b39a83db166
d1618033 <d1618033@gmail.com>
2015-03-18 21:31:25 +0200

    add test for n = 1

--- test_word_search.py
+++ test_word_search.py
@@ -12,3 +12,12 @@ class TestLettersInDir(unittest.TestCase):
         expected = [5, 2]
         actual = word_search.letters_in_dir(self.board, i, j, n, direction)
         self.assertEqual(actual, expected)
+
+    def test_n_1_always_returns_number(self):
+        n = 1
+        for i in range(3):
+            for j in range(3):
+                for direction in word_search.DIRECTIONS:
+                    expected = [self.board[i][j]]
+                    actual = word_search.letters_in_dir(self.board, i, j, n, direction)
+                    self.assertEqual(actual, expected)
